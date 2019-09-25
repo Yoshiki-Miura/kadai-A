4446a7b335ef0f9aa3faa5395609a79572b19815
yoshiki-san <yy.bb.tf@gmail.com>
2019-09-25 11:26:09 +0900

    int関数の存在を忘れてた

--- basic_statistics.py
+++ basic_statistics.py
@@ -10,10 +10,33 @@ def max2(mylist):
     temp = mylist[0]
     for i in range(1, len(mylist)):
         if temp < mylist[i]:
-            temp = mylist[]
+            temp = mylist[i]
+
+    return temp
+
+
+def min2(mylist):
+    temp = mylist[0]
+    for i in range(1, len(mylist)):
+        if temp > mylist[i]:
+            temp = mylist[i]
+
+    return temp
+
+
+def ave(mylist):
+    return sum2(mylist) / len(mylist)
 
 
 def main():
+    mylist = input("データを入力してください（スペース区切り）> ").split()
+    for i in range(len(mylist)):
+        mylist[i] = int(mylist[i])
+
+    print("合計値: " + str(sum2(mylist)))
+    print("最大値: " + str(max2(mylist)))
+    print("最小値: " + str(min2(mylist)))
+    print("平均値: " + str(ave(mylist)))
 
 
 if __name__ == '__main__':
