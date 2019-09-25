72fe86492dbf6a9f21d7b1ae0a805659359ced39
yoshiki-san <yy.bb.tf@gmail.com>
2019-09-25 09:52:28 +0900

    pythonではprintに勝手に改行が入るから大変

---
+++ kuku-1.py
@@ -0,0 +1,11 @@
+def main():
+    for i in range(1, 10):
+        for j in range(1, 10):
+            if j == 9:
+                print(i * j)
+            else:
+                print(f"{i*j} ", end="")
+
+
+if __name__ == '__main__':
+    main()
